739be2fa76a3c9986db210b6bdf6d58df11d74d0
Anshul-04 <anshulonmars@gmail.com>
2024-02-08 03:09:12 +0530

    Use products file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,30 +1,4 @@
-const products = [{
-  image :'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name : 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  ratings : {
-    stars :4.5,
-    count : 87
-  },
-  priceCents : 1090
-},{
-  image :'images/products/intermediate-composite-basketball.jpg',
-  name : 'Intermediate Size Basketball',
-  ratings : {
-    stars :4,
-    count : 127
-  },
-  priceCents : 2095
-},{
-  image :'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name : 'Adults Plain Cotton T-Shirt - 2 Pack',
-  ratings : {
-    stars :4.5 ,
-    count : 56
-  },
-  priceCents : 799
-}];
-
-
+  
 let productsHTML = '';
 // for each product we will create HTML
 products.forEach((product) => {
@@ -42,9 +16,9 @@ products.forEach((product) => {
 
       <div class="product-rating-container">
         <img class="product-rating-stars"
-          src="images/ratings/rating-${product.ratings.stars * 10}.png">
+          src="images/ratings/rating-${product.rating.stars * 10}.png">
         <div class="product-rating-count link-primary">
-          ${product.ratings.count}
+          ${product.rating.count}
         </div>
       </div>
 
@@ -81,6 +55,6 @@ products.forEach((product) => {
   `;
 });
 
-console.log(productsHTML);
+// console.log(productsHTML);
 
-document.querySelector('.js-product-grid').innerHTML = productsHTML ;
+document.querySelector('.js-products-grid').innerHTML = productsHTML ;
